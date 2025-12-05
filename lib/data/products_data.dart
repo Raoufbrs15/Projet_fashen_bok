6d6f8ae348a812b0005a39904e68e584b0319067
makoudimassinissa <makoudimassimtts@gmail.com>
2025-12-04 20:28:49 -0500

    data,products_data

---
+++ lib/data/products_data.dart
@@ -0,0 +1,105 @@
+import '../models/product.dart';
+
+final List<Product> tshirtProducts = [
+  Product(
+    id: 't1',
+    name: 'T-shirt classique',
+    price: 19.99,
+    imagePath: 'assets/images/tshirt01.png',
+  ),
+  Product(
+    id: 't4',
+    name: 'T-shirt logo',
+    price: 19.99,
+    imagePath: 'assets/images/tshirt02.png',
+  ),
+  Product(
+    id: 't2',
+    name: 'T-shirt graphique',
+    price: 17.99,
+    imagePath: 'assets/images/tshirt03.png',
+  ),
+  Product(
+    id: 't3',
+    name: 'T-shirt rayé',
+    price: 18.50,
+    imagePath: 'assets/images/tshirt04.png',
+  ),
+  Product(
+    id: 't3',
+    name: 'col boutonné',
+    price: 18.50,
+    imagePath: 'assets/images/tshirt05.png',
+  ),
+];
+
+final List<Product> shoesProducts = [
+  Product(
+    id: 's1',
+    name: 'Basket sport',
+    price: 59.99,
+    imagePath: 'assets/images/shoes01.png',
+  ),
+  Product(
+    id: 's2',
+    name: 'Running léger',
+    price: 79.50,
+    imagePath: 'assets/images/shoes03.png',
+  ),
+  Product(
+    id: 's3',
+    name: 'Basket polyvalent',
+    price: 29.99,
+    imagePath: 'assets/images/shoes04.png',
+  ),
+  Product(
+    id: 's3',
+    name: 'Sneaker moderne',
+    price: 29.99,
+    imagePath: 'assets/images/shoes05.png',
+  ),Product(
+    id: 's3',
+    name: 'Chaussure de course',
+    price: 29.99,
+    imagePath: 'assets/images/shoes06.png',
+  )
+];
+
+final List<Product> pantalonProducts = [
+  Product(
+    id: 'p1',
+    name: 'Jean bleu',
+    price: 49.99,
+    imagePath: 'assets/images/pantalon01.png',
+  ),
+  Product(
+    id: 'p2',
+    name: 'Jogger sport',
+    price: 39.99,
+    imagePath: 'assets/images/pantalon02.png',
+  ),
+  Product(
+    id: 'p3',
+    name: 'Jean coupe',
+    price: 44.50,
+    imagePath: 'assets/images/pantalon03.png',
+  ),
+  Product(
+    id: 'p1',
+    name: 'Pantalon chino beige',
+    price: 49.99,
+    imagePath: 'assets/images/pantalon04.png',
+  ),
+  Product(
+    id: 'p1',
+    name: 'Pantalon de costume',
+    price: 49.99,
+    imagePath: 'assets/images/pantalon05.png',
+  ),
+  Product(
+    id: 'p1',
+    name: 'Pantalon noir',
+    price: 49.99,
+    imagePath: 'assets/images/pantalon06.png',
+  )
+];
